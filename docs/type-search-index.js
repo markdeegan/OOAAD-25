2149e34f57cd8d9c77148a8d2b9e10897b128843
Mark Deegan <mark.deegan@tudublin.ie>
2025-10-16 16:06:35 +0100

    Adding first versioj of Java Docs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"car"},{"p":"<Unnamed>","l":"fleet"},{"p":"<Unnamed>","l":"vehicle"}];updateSearchResults();
