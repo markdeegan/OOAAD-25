e572dc51029c734dbcc448303bd1d7b10133d914
Mark Deegan <1252943+markdeegan@users.noreply.github.com>
2025-11-05 17:18:21 +0000

    Updating some examples for car and fleet

--- src/java/car.java
+++ src/java/car.java
@@ -1,7 +1,30 @@
+////////// ////////// ////////// //////////
+// Mark Deegan
+// Thu Oct 16 16:09:52 IST 2025
+// DTAI H3013 Object Oriented Analysis & Design
+////////// ////////// ////////// //////////
+ 
+ 
+ 
+/** 
+ * @author Mark Deegan 
+ */
 public class car extends vehicle
-{
-	public void beep()
+/** class to represent a car, which is an extension of or a 
+ * specialisaiton of class vehicle */
+{ // start definition of the class car
+	String VIN;
+	public car()
 	{
-		System.out.println("Beep! Beep!");
 	}
-}
+
+	public car(String paramVIN)
+	{
+		VIN = paramVIN;
+	}
+	public void beep()
+	/** public method beep to make the car sound the horn */
+	{ // end definition of method car::beep()
+		System.out.println("Beep! Beep!");
+	} // end definition of method cat::beep()
+} // end definition of the class car
