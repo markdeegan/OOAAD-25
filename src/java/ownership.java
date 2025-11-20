31bb0f6c57d4a4fdceaf0ef508ffbbe6296ba659
Mark Deegan <1252943+markdeegan@users.noreply.github.com>
2025-11-20 15:07:47 +0000

    Update ownership.java

--- src/java/ownership.java
+++ src/java/ownership.java
@@ -4,4 +4,31 @@
 public class ownership {
 	private person vehicleOwner;
 	private vehicle ownedVehicle;
+
+	public void setOwner(person owner) {
+		this.vehicleOwner = owner;
+	}	
+
+	public person getOwner() {
+		return this.vehicleOwner;
+	}	
+
+	public void setVehicle(vehicle vehicle) {
+		this.ownedVehicle = vehicle;
+	}	
+
+	public vehicle getVehicle() {
+		return this.ownedVehicle;
+	}	
+
+	public static void main(String[] args) {
+		person alice = new person("Alice");
+		vehicle car = new vehicle();
+
+		ownership ownershipRecord = new ownership();
+		ownershipRecord.setOwner(alice);
+		ownershipRecord.setVehicle(car);
+
+		System.out.println(ownershipRecord.getOwner()+ " owns the following vehicle " + ownershipRecord.getVehicle());
+	}
 }
