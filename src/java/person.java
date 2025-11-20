81d0cdff75fcfbf9b20347ca731b0f0a2d8f1259
TristanBerns <b00161402@mytudublin.ie>
2025-11-20 20:16:07 +0000

    Update person.java

--- src/java/person.java
+++ src/java/person.java
@@ -1,27 +1,25 @@
-import java.sql.Date;
+import java.sql.Date;                               // imports the Date class for storing the date of birth
 
-public class person {
-   private String placeOfBirth;
-   private Date dateOfBirth;
-    private String givenName;
-    private String familyName;
+public class person {                               // sefines person class
+   private String placeOfBirth;                     // stores where the person was born (doesn't change)
+   private Date dateOfBirth;                        // stores the person's date of birth (doesn't change)
+   private String givenName;                        // stores the person's given/birth name
+   private String familyName;                       // stores the person's family/birth name
 
-
-    public person(String pob) {
-        this.placeOfBirth = pob;
+    public person(String pob) {                     // constructor that takes place of birth as input
+        this.placeOfBirth = pob;                    // sets the placeOfBirth field to the given value
     }
 
-    public String getGivenName() {
-        return givenName;
+    public String getGivenName() {                  // returns the person's given name
+        return givenName;                           // read only access to givenName
     }
-    public String getFamilyName() {
-        return familyName;
+    public String getFamilyName() {                 // returns the person's family name
+        return familyName;                          // read only access to familyName
     }
 
-    public static void main(String[] args) {
-        // Example usage
-        person p = new person("New York"); // 
+    public static void main(String[] args) {        // main method for testing the class
+        person p = new person("New York");     // creates a new person object with place of birth "New York"
         System.out.println("Place of Birth: " + p.placeOfBirth);
-    }   
+    }                                              // prints the place of birth for the created person
 }
 
