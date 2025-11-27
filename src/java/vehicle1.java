e40be544b92301f7e03e34f2e5dae0aa3a4fc335
Aarti Saravanan <B00161946@MYTUDUBLIN.IE>
2025-11-27 10:52:05 +0000

    Update vehicle1.java

--- src/java/vehicle1.java
+++ src/java/vehicle1.java
@@ -3,25 +3,28 @@
 //programme to show vehicle classes (e.g. car, plane, boat)
 //Thursday 20 November 2025
 
+//This class represents a vehicle with a model, make and VIN
+public class vehicle {
+    private String CarModel; //Model of the car
+    private String CarMake; //Make of the Car
+    private int VehicleIdentificationNumber; // Unique ID Number
 
-public class vehilcle {
-    private String CarModel;
-    private String CarMake;
-    private int VehicleIdentificationNumber;
-
-
-    public vehicle(String VIN){
-        this.VehicleIdentificationNumber = VIN;
+    //Constructor that accepts the VIN as an integer
+    public vehicle(int VIN){
+        this.VehicleIdentificationNumber = VIN; //Set the VIN when creating the object
     }
 
+    //Get method for the car model
     public String getCarModel(){
         return CarModel;
     }
 
+    //Get method for the car make
     public String getCarMake(){
         return CarMake;
     }
 
+    //Get method for the VIN
     public int getVIN(){
         return VIN;
     }
