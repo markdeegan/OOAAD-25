ff6d2382c6baa45d19fd7566c1f1cd0558037ecd
Aarti Saravanan <B00161946@MYTUDUBLIN.IE>
2025-12-09 16:25:05 +0000

    Create Windsurfer.java

---
+++ src/java/Windsurfer.java
@@ -0,0 +1,25 @@
+// Aarti Saravanan
+//OOATD
+// DTAI Year 3
+//AS20251209
+//Windsurfer Class
+
+// Declares a public class named Windsurfer that extends NonPoweredWaterVehicle and implements the asset interface
+public class Windsurfer extends NonPoweredWaterVehicle implements asset{
+
+// Declares a private float command to store the value of the Windsurfer
+private float value;
+
+// Public method to set the value of the Windsurfer
+// Takes a float input representing the new value
+public void setValue(float value){
+    this.value = value;  // Assigns the input value to the instance variable 'value'
+                         // 'this' refers to the current object instance
+}
+
+public string toString(){  // Public method that returns a string to represent the Windsurfer object
+    return "Windsurfer, Value: $" + value;  // Returns a formatted string containing the class name and value
+                                          // The value is displayed with a doller sign
+
+}
+}
