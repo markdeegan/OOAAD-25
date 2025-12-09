0e53ad27fe329ffd50cbba257468476c9f946937
Aarti Saravanan <B00161946@MYTUDUBLIN.IE>
2025-12-09 16:02:59 +0000

    Create Rocket.java

---
+++ src/java/Rocket.java
@@ -0,0 +1,25 @@
+// Aarti Saravanan
+//OOATD
+// DTAI Year 3
+//AS20251209
+//Rocket Class
+
+// Declares a public class named Rocket that extends PoweredAirVehicle and implements the asset interface
+public class Rocket extends PoweredAirVehicle implements asset{
+
+// Declares a private float command to store the value of the Rocket
+private float value;
+
+// Public method to set the value of the Rocket
+// Takes a float input representing the new value
+public void setValue(float value){
+    this.value = value;  // Assigns the input value to the instance variable 'value'
+                         // 'this' refers to the current object instance
+}
+
+public string toString(){  // Public method that returns a string to represent the Rocket object
+    return "Rocket, Value: $" + value;  // Returns a formatted string containing the class name and value
+                                          // The value is displayed with a doller sign
+
+}
+}
