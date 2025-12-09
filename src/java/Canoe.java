b25e78bd8bc613a17ca6ae0dcc54fe9c194b70d4
Aarti Saravanan <B00161946@MYTUDUBLIN.IE>
2025-12-09 16:24:27 +0000

    Create Canoe.java

---
+++ src/java/Canoe.java
@@ -0,0 +1,25 @@
+// Aarti Saravanan
+//OOATD
+// DTAI Year 3
+//AS20251209
+//Canoe Class
+
+// Declares a public class named Canoe that extends NonPoweredWaterVehicle and implements the asset interface
+public class Canoe extends NonPoweredWaterVehicle implements asset{
+
+// Declares a private float command to store the value of the Canoe
+private float value;
+
+// Public method to set the value of the Canoe
+// Takes a float input representing the new value
+public void setValue(float value){
+    this.value = value;  // Assigns the input value to the instance variable 'value'
+                         // 'this' refers to the current object instance
+}
+
+public string toString(){  // Public method that returns a string to represent the Canoe object
+    return "Canoe, Value: $" + value;  // Returns a formatted string containing the class name and value
+                                          // The value is displayed with a doller sign
+
+}
+}
