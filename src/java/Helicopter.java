8f4dee68a39cc586a35a85caec796a4564afcea1
Aarti Saravanan <B00161946@MYTUDUBLIN.IE>
2025-12-10 15:19:38 +0000

    Update Helicopter.java

--- src/java/Helicopter.java
+++ src/java/Helicopter.java
@@ -4,22 +4,10 @@
 //AS20251209
 //Helicopter Class
 
-// Declares a public class named Helicopter that extends PoweredAirVehicle and implements the asset interface
-public class Helpicopter extends PoweredAirVehicle implements asset{
-
-// Declares a private float command to store the value of the Helicopter
-private float value;
-
-// Public method to set the value of the Helicopter
-// Takes a float input representing the new value
-public void setValue(float value){
-    this.value = value;  // Assigns the input value to the instance variable 'value'
-                         // 'this' refers to the current object instance
-}
-
-public string toString(){  // Public method that returns a string to represent the Helicopter object
-    return "Helicopter, Value: $" + value;  // Returns a formatted string containing the class name and value
-                                          // The value is displayed with a doller sign
-
-}
+public class Helicopter extends AirVehicle { //Rotary Wing aircraft class
+    
+    public Helicopter() {
+        super(1, "rotary-wing", 10000); // Constructor: 1 main rotor, rotary-wing type, 10,000 ft max altitude
+        System.out.println("Helicopter created."); // Output creation
+    }
 }
