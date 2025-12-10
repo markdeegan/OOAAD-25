807fa01ae368c7217675bac50141ae2e2344bb47
Aarti Saravanan <B00161946@MYTUDUBLIN.IE>
2025-12-10 15:33:30 +0000

    Update Sailboat.java

--- src/java/Sailboat.java
+++ src/java/Sailboat.java
@@ -4,22 +4,9 @@
 //AS20251209
 //Sailboat Class
 
-// Declares a public class named Sailboat that extends NonPoweredWaterVehicle and implements the asset interface
-public class Sailboat extends NonPoweredWaterVehicle implements asset{
-
-// Declares a private float command to store the value of the Sailboat
-private float value;
-
-// Public method to set the value of the Sailboat
-// Takes a float input representing the new value
-public void setValue(float value){
-    this.value = value;  // Assigns the input value to the instance variable 'value'
-                         // 'this' refers to the current object instance
-}
-
-public string toString(){  // Public method that returns a string to represent the Sailboat object
-    return "Sailboat, Value: $" + value;  // Returns a formatted string containing the class name and value
-                                          // The value is displayed with a doller sign
-
-}
+public class Sailboat extends WaterVehicle { // NonPowered watercraft class
+    public Sailboat() {
+        super(1, "wind-powered"); //Constructor: wind-powered
+        System.out.println("Sailboat created."); //Output creation
+    }
 }
