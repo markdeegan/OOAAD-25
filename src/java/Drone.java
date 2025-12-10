c79beba53e954f37ae75e3bf03cfa2051cfdc6be
Aarti Saravanan <B00161946@MYTUDUBLIN.IE>
2025-12-10 15:19:33 +0000

    Update Drone.java

--- src/java/Drone.java
+++ src/java/Drone.java
@@ -4,22 +4,10 @@
 //AS20251209
 //Drone Class
 
-// Declares a public class named Drone that extends PoweredAirVehicle and implements the asset interface
-public class Drone extends PoweredAirVehicle implements asset{
-
-// Declares a private float command to store the value of the Drone
-private float value;
-
-// Public method to set the value of the Drone
-// Takes a float input representing the new value
-public void setValue(float value){
-    this.value = value;  // Assigns the input value to the instance variable 'value'
-                         // 'this' refers to the current object instance
-}
-
-public string toString(){  // Public method that returns a string to represent the Drone object
-    return "Drone, Value: $" + value;  // Returns a formatted string containing the class name and value
-                                          // The value is displayed with a doller sign
-
-}
+public class Drone extends AirVehicle { // Electric aircraft class
+    
+    public Drone() {
+        super(4, "electric", 400); // Constructor: 4 motors, electric, 400 ft max altitude
+        System.out.println("Drone created."); // Output creation
+    }
 }
